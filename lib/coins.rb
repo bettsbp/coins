815587cc80fea02a9a2ff7f461fc501ef05bad88
Brandon Betts and Rita Bennett-Chew <brandon+rita@>
2018-06-28 09:58:17 -0700

    Add code and some passing tests

--- lib/coins.rb
+++ lib/coins.rb
@@ -10,4 +10,27 @@ class Amount
   def get_cents
     @amount * 100
   end
+
+  def get_coins
+    new_amount = self.get_cents
+    qtrs = 0
+    totalArray = []
+    if new_amount >= 25
+      qtrs = (new_amount / 25).floor
+      left_over_after_qtrs = new_amount % 25
+    elsif new_amount < 25
+      if left_over_after_qtrs >= 10
+        dimes = (left_over_after_qtrs / 10).floor
+        left_over_after_dimes = left_over_after_qtrs % 10
+      end
+    end
+    totalArray.push(qtrs)
+    totalArray.push(dimes)
+  end
+
+  def add_coins
+    finalArray = []
+    finalArray.push(@qtr)
+    finalArray.push(@dimes)
+  end
 end
--- spec/coins_spec.rb
+++ spec/coins_spec.rb
@@ -11,4 +11,12 @@ describe("Coins#count") do
     amount = Amount.new(0.75)
     expect(amount.get_cents).to(eq(75))
   end
+  it("takes .75 and returns 3 using get_coins") do
+    amount = Amount.new(0.75)
+    expect(amount.get_coins).to(eq([3, 0]))
+  end
+  it("takes .85 and returns 3 using get_coins") do
+    amount = Amount.new(0.85)
+    expect(amount.get_coins).to(eq([3, 1]))
+  end
 end
